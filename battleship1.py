b5f6fff20069fce49b9e587138782e6020fec55b
gergodanko <55234339+gergodanko@users.noreply.github.com>
2019-09-16 15:11:15 +0200

    Add files via upload

--- battleship1.py
+++ battleship1.py
@@ -3,16 +3,25 @@ for i in range(10):
     board.append(["0"]*10)
 board1=[]
 for i in range(10):
-    board.append(["0"]*10)
-while True:
-    try:
-        while ship:
-            ship=(int(input("Where should your ship be? Row ")))
-        while ship[1]>5 or ship[1]==0:
+    board1.append(["0"]*10)
+print(board1)
 
+#while True:
+    #try:
+ship1=-1  
+ship2=-1
+while ship1>10 or ship1<0:
+    ship1=(int(input("Where should your ship be? Row ")))
+while ship2>10 or ship2<0:
+    ship2=(int(input("Where should you ship be? Column ")))
+    #except ValueError:
+        #print("Wrong input!")
+for i in range(5):
+    board1[ship1][ship2]="1"
+    ship1+=1
 
-    except ValueError:
-        print("Wrong input!")
-for row in board:
+for row in board1:
     print(" ".join(row))
-print(ship)
+#for row in board:
+ #   print(" ".join(row))
+
