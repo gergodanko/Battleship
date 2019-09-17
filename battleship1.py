7bafbf252bb08e5d946bcec4c842349b07285acf
istvanpalasthy <55269040+istvanpalasthy@users.noreply.github.com>
2019-09-17 11:52:55 +0200

    Add files via upload

--- battleship1.py
+++ battleship1.py
@@ -1,3 +1,4 @@
+
 board=[]
 for i in range(10):
     board.append(["0"]*10)
@@ -5,40 +6,58 @@ board1=[]
 for i in range(10):
     board1.append(["0"]*10)
 
-
-while True:
-    try:
-        ship1=-1  
-        ship2=-1
-        a=""
-        a=str(input("Milyen irányba álljon a hajo? u,d,l,r"))
+ship1=-1  
+ship2=-1
+a=""
+a=str(input("Milyen irányba álljon a hajo? u,d,l,r"))
+if a=="u":
+    while ship1<5 or ship1<0:
+        ship1=(int(input("Where should your ship be? Row ")))
+elif a=="d":
+    while ship1>5 or ship1<0:
+        ship1=(int(input("Where should your ship be? Row ")))
+elif a=="l" or a=="r":
+    while ship1>10 or ship1<0:
+        ship1=(int(input("Where should your ship be? Row ")))
+if a=="u" or a=="d":
+    while ship2>10 or ship2<0:
+        ship2=(int(input("Where should you ship be? Column ")))
+elif a=="l":
+    while ship2<5 or ship2>10:
+        ship2=(int(input("Where should you ship be? Column ")))
+elif a=="r":
+    while ship2>5 or ship2<0:
+        ship2=(int(input("Where should you ship be? Column ")))
+for i in range(5):
+    if a=="u":
+        board1[ship1][ship2]="1"
+        ship1+=1
+    elif a=="d":
+        board1[ship1][ship2]="1"
+        ship1-=1
+    elif a=="l":
+        board1[ship1][ship2]="1"
+        ship2-=1
+    elif a=="r":
+        board1[ship1][ship2]="1"
+        ship2+=1
+def shipsize(n):
+    for i in range(n):
         if a=="u":
-            while ship1<5 or ship1<0:
-                ship1=(int(input("Where should your ship be? Row ")))
+            board1[ship1][ship2]="1"
+            ship1+=1
         elif a=="d":
-            while ship1>5 or ship1<0:
-                ship1=(int(input("Where should your ship be? Row ")))
-        elif a=="l" or a=="r":
-            while ship1>10 or ship1<0:
-                ship1=(int(input("Where should your ship be? Row ")))
-        if a=="u" or a=="d":
-            while ship2>10 or ship2<0:
-                ship2=(int(input("Where should you ship be? Column ")))
+            board1[ship1][ship2]="1"
+            ship1-=1
         elif a=="l":
-            while ship2<5 or ship2>10:
-                ship2=(int(input("Where should you ship be? Column ")))
+            board1[ship1][ship2]="1"
+            ship2-=1
         elif a=="r":
-            while ship2>5 or ship2<0:
-                ship2=(int(input("Where should you ship be? Column ")))
-        for i in range(5):
-            if a=="u":
-                board1[ship1][ship2]="1"
-                ship1-=1
-            elif 
-        break
+            board1[ship1][ship2]="1"
+            ship2+=1
+shipsize(4)
 
-    except ValueError:
-        print("Wrong input!")
+
 #for i in range(5):
    # board1[ship1][ship2]="1"
     #ship1+=1
@@ -47,4 +66,3 @@ for row in board1:
     print(" ".join(row))
 #for row in board:
  #   print(" ".join(row))
-
