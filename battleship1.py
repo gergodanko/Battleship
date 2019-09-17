28d5f86d78faabc149f51755af700347f31978f5
gergodanko <55234339+gergodanko@users.noreply.github.com>
2019-09-17 11:14:02 +0200

    Add files via upload

--- battleship1.py
+++ battleship1.py
@@ -12,13 +12,29 @@ while True:
         ship2=-1
         a=""
         a=str(input("Milyen irányba álljon a hajo? u,d,l,r"))
-        while ship1<5 or ship1<0:
-            ship1=(int(input("Where should your ship be? Row ")))-1
-        while ship2>10 or ship2<0:
-            ship2=(int(input("Where should you ship be? Column ")))-1
+        if a=="u":
+            while ship1<5 or ship1<0:
+                ship1=(int(input("Where should your ship be? Row ")))
+        elif a=="d":
+            while ship1>5 or ship1<0:
+                ship1=(int(input("Where should your ship be? Row ")))
+        elif a=="l" or a=="r":
+            while ship1>10 or ship1<0:
+                ship1=(int(input("Where should your ship be? Row ")))
+        if a=="u" or a=="d":
+            while ship2>10 or ship2<0:
+                ship2=(int(input("Where should you ship be? Column ")))
+        elif a=="l":
+            while ship2<5 or ship2>10:
+                ship2=(int(input("Where should you ship be? Column ")))
+        elif a=="r":
+            while ship2>5 or ship2<0:
+                ship2=(int(input("Where should you ship be? Column ")))
         for i in range(5):
-            board1[ship1][ship2]="1"
-            ship1-=1
+            if a=="u":
+                board1[ship1][ship2]="1"
+                ship1-=1
+            elif 
         break
 
     except ValueError:
